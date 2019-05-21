2c041c8a969251b6f9eecc72c198f3a6c94d952d
John <johnbaironlr@gmail.com>
2019-05-21 18:43:52 -0500

    Posts-CRUD

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def form_title
+    @post.new_record? ? "Publicar Producto" : "Modificar Producto"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/show'
+  root 'posts#index'
+
   devise_for :users
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
