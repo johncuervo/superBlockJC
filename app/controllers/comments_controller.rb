ace0b8a047b05c1930769f8c8e208c4d4d654a26
John <johnbaironlr@gmail.com>
2019-06-11 22:41:05 -0500

    Comments-AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,47 @@
 class CommentsController < ApplicationController
-  def create
-    post = Post.find(params[:post_id])
-    post.comments.create(comments_params)
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
-    redirect_to post
+  def new
+    @comment = Comment.new
   end
 
-private
-  def comments_params
-    params.require(:comment).permit(:content).merge(user: current_user)
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.js # renders create.js.erb
+      else
+        format.js
+      end
+    end
+  end
+
+  def edit
+  end
+
+  # def update
+  #   if @comment.update (comment_params)
+  #     flash[:notice] = "Comentario editado exitosamente."
+  #     redirect_to post_path(id: params[:comment][:post_id])
+  #   else
+  #     render post
+  #   end
+  # end
+
+  def destroy
+
+    @comment.destroy
+    redirect_to post_path( @comment.post_id), notice: 'Comentario eliminado exitosamente'
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-  resources :posts do
-    resources :comments, only: [:create]
-  end
+  resources :posts
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
